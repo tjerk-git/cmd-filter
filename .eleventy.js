d64e624e30eb1927141601f15bcc4a5fd476b09c
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2023-09-04 17:03:34 +0200

    yay!

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,53 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/sass/");
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy("./src/js");
-  // eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.setTemplateFormats([
+    // Templates:
+    "html",
+    "njk",
+    "md",
+    "css",
+  ]);
+  eleventyConfig.addPassthroughCopy("public");
+
+  eleventyConfig.addPassthroughCopy("bundle.js");
+
+  eleventyConfig.addNunjucksFilter("excludeFromCollection", function (collection = [], pageUrl = this.ctx.page.url) {
+    return collection.filter(post => post.url !== pageUrl);
+  });
+
+  eleventyConfig.addFilter("filterByTags", function (collection = [], ...requiredTags) {
+    return collection.filter(post => {
+      return requiredTags.flat().every(tag => post.data.tags?.includes(tag));
+    });
+  });
+
+  eleventyConfig.addNunjucksFilter("related", function (collection = []) {
+    const { tags: requiredTags, page } = this.ctx;
+    return collection.filter(post => {
+      return post.url !== page.url && requiredTags?.every(tag => post.data.tags?.includes(tag));
+    });
+  });
+
+  eleventyConfig.addCollection("teachers", function (collection) {
+
+    const coll = collection
+      .getFilteredByTag("teachers");
+
+    for (let i = 0; i < coll.length; i++) {
+      const prevPost = coll[i - 1];
+      const nextPost = coll[i + 1];
+
+      coll[i].data["prevPost"] = prevPost;
+      coll[i].data["nextPost"] = nextPost;
+    }
+
+    return coll;
+  });
+
   return {
     dir: {
       input: "src",
-      output: "public",
-    },
+      includes: "_includes",
+      output: "build"
+    }
   };
 };
---
+++ build/public/bundle.js
@@ -0,0 +1,25 @@
+const tags = document.querySelectorAll('.teacher__tags-tag');
+const posts = document.querySelectorAll('.teacher');
+const selectedTags = new Set();
+
+tags.forEach(tag => {
+  tag.addEventListener('click', () => {
+    const selectedTag = tag.getAttribute('data-tag');
+
+    // Toggle the selection state of the tag
+    if (selectedTags.has(selectedTag)) {
+      selectedTags.delete(selectedTag);
+      tag.classList.remove('selected');
+    } else {
+      selectedTags.add(selectedTag);
+      tag.classList.add('selected');
+    }
+
+    // Filter posts based on selected tags
+    posts.forEach(post => {
+      const postTags = post.getAttribute('data-tags').split(' ');
+      const isVisible = Array.from(selectedTags).every(tag => postTags.includes(tag));
+      post.style.display = isVisible ? 'block' : 'none';
+    });
+  });
+});
