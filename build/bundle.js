5dc7313969fdac6a0692f6568712174d79b4ae70
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2023-09-07 17:59:55 +0200

    yeah

--- build/bundle.js
+++ build/bundle.js
@@ -2,7 +2,19 @@ const tags = document.querySelectorAll('.teacher__tags-tag');
 const teachers = document.querySelectorAll('.teacher');
 const selectedTags = new Set();
 
+// Get a reference to the anchor element
+const anchor = document.getElementById("anchor");
 
+// Add an event listener for the "Escape" key press
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    console.log("Escape key pressed");
+    // Prevent the default "Esc" behavior (e.g., closing a modal)
+    event.preventDefault();
+
+    window.location.hash = "#";
+  }
+});
 
 tags.forEach(tag => {
   tag.addEventListener('click', () => {
